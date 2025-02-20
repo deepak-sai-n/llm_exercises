db743adb908d07cbcf2708001469b62a81936d2a
deepak-sai-n <89226191+deepak-sai-n@users.noreply.github.com>
2025-02-20 00:19:51 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_helper import get_few_shot_db_chain
 
-st.title("AtliQ T Shirts: Database Q&A 👕")
+st.title("Retail: Database Q&A")
 
 question = st.text_input("Question: ")
 
